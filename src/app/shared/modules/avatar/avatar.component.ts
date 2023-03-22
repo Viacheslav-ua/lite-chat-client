829f0f49216e2d5174b34b441cb11c52022d722b
Viacheslav-ua <atph@ukr.net>
2023-03-22 08:15:35 +0200

    added Avatar component

---
+++ src/app/shared/modules/avatar/avatar.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AvatarComponent {
+
+}
